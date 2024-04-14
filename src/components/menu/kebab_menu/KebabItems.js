def461ec34e5775eed80da84556361178d4dfc24
Oleksandr Poliakov <poliakovaleek@gmail.com>
2024-04-14 22:19:21 +0200

    Small menu fix

--- src/components/menu/kebab_menu/KebabItems.js
+++ src/components/menu/kebab_menu/KebabItems.js
@@ -3,8 +3,24 @@ import { useNavigate } from 'react-router-dom';
 
 const KebabItems = (props) => {
 
+    const kebabClick=()=>{
+        const element = document.getElementsByClassName('kebab')[0];
+        const classN = 'clicked';
+        if(element){
+            if (element.classList.value.includes(classN)){
+                element.classList.remove(classN);
+                //steItems(undefined);
+            }else{
+                element.classList.add(classN);
+               // steItems(props.items);
+            } 
+    
+        }
+    }
+
     const navigate = useNavigate();
     const clickHandler = (event) => {
+        kebabClick();
         navigate(event.target.getAttribute('path'), { replace: true });
     };
 
