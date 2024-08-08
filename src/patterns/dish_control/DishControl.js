1af516a79b9fb5195ed8d321ef1eb8cb60f323fd
Oleksandr Poliakov <poliakovaleek@gmail.com>
2024-08-08 12:47:23 +0200

    Small cosmetic changes

--- src/patterns/dish_control/DishControl.js
+++ src/patterns/dish_control/DishControl.js
@@ -1,5 +1,4 @@
 import { IconButton } from '@mui/material';
-import './DishControl.css'
 import { DeleteOutlined } from '@mui/icons-material';
 import { styled } from '@mui/material/styles';
 
@@ -17,12 +16,6 @@ const DishControl = (props) => {
     return <StyledButton id={props.id} food={props.food} onClick={props.onRemoveClick}>
         <DeleteOutlined />
     </StyledButton>
-
-    return <div id={props.id} food={props.food} className='dish_control'>
-        <button className='dish_control__btn' onClick={props.onRemoveClick}>
-            <img src="rem.png" alt="buttonpng" border="0" />
-        </button>
-    </div>
 }
 
 export default DishControl;
